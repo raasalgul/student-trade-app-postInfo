a5b2b4fae8e964edded9fbb5b40661cf14124440
raasalgul <k.sathishrsurya@gmail.com>
2021-12-03 00:28:36 +0000

    add post

--- postInfo/__init__.py
+++ postInfo/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from postInfo import getAllPostInfo,getPostInfo
+from postInfo import getAllPostInfo,getPostInfo,addAccommodation,addJob,addQandA,addOldProducts,addOtherServices
 # updateUserInfo,uploadPicture,verificationDoc
---
+++ postInfo/addAccommodation.py
@@ -0,0 +1,49 @@
+from flask import request
+from postInfo import app
+import boto3
+import logging
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import json
+
+''' Loading Environment files '''
+load_dotenv()
+
+''' Configuring AWS dynamo db '''
+dynamoDbResource = boto3.resource(os.getenv("AWS_DYNAMO"), region_name=os.getenv("AWS_REGION"))
+''' Configuring AWS Cognito '''
+cognitoClient = boto3.client(os.getenv("AWS_COGNITO"), region_name=os.getenv("AWS_REGION"))
+table_name = os.getenv("DYNAMO_ACCOMMODATION_TABLE")
+
+'''signUp method will add a record to AWS Cognito and then add those info to User Info Dynamo DB, before
+adding to the cognito we will do a check if the email id is already present in the dynamo DB'''
+
+
+@app.route('/add-accommodation', methods=['POST'])
+def addAccommodation():
+    # logging.log("addAccommodation() request is "+json.dumps(request.get_json()))
+    response = {}
+    try:
+        '''Connect to the User Info table'''
+        table = dynamoDbResource.Table(table_name)
+        # logging.log("Table is connected")
+        item = {"name": request.json['name'],
+                "email": request.json['email'],
+                "eirCode": request.json['eirCode'],
+                "addedDateTime":datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+                "area":request.json['area'],
+                "rent": request.json['rent'],
+                "institution": request.json['institution'],
+                "description": request.json['description'],
+                "picture": request.json['picture'],
+                "duration": request.json['duration'],
+                "availability": request.json['availability']
+                }
+        '''After adding to the cognito pool add the basic info to user info table'''
+        response=table.put_item(Item=item)
+        # logging.log("New User added to the Dynamo Db")
+    except ClientError as e:
+        logging.error(e)
+    return response
