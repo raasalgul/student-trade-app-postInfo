6a39837f623c283589664b5f2144144415e4cd88
raasalgul <k.sathishrsurya@gmail.com>
2021-12-13 01:43:41 +0000

    cros error

--- postInfo/__init__.py
+++ postInfo/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+from flask_cors import CORS
+
 application = Flask(__name__)
+CORS(application)
 
 from postInfo import getAllPostInfo,getPostInfo,addAccommodation,addJob,addQandA,addOldProducts,addOtherServices
